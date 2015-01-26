489f82787d95c4884e1533ac5bdf1a1597d80f18
Will Shackleton <w.shackleton@gmail.com>
2015-01-26 15:50:08 +0000

    Added application entrypoint stub

---
+++ src/main/java/uk/ac/cam/cl/wildpetscience/triton/client/Client.java
@@ -0,0 +1,10 @@
+package uk.ac.cam.cl.wildpetscience.triton.client;
+
+/**
+ * The entry point of the client application
+ */
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Stub");
+    }
+}
