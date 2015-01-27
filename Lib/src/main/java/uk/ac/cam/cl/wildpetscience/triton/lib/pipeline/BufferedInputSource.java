90919f5462ff36f068d26d0a71db160a41ed83af
Will Shackleton <w.shackleton@gmail.com>
2015-01-27 19:58:50 +0000

    Added BufferedInputSource

---
+++ Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/pipeline/BufferedInputSource.java
@@ -0,0 +1,65 @@
+package uk.ac.cam.cl.wildpetscience.triton.lib.pipeline;
+
+import uk.ac.cam.cl.wildpetscience.triton.lib.image.Image;
+
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * An ImageInputSource that fetches input ahead of time and buffers input.
+ */
+public class BufferedInputSource implements ImageInputSource {
+    private final ImageInputSource source;
+    private final BlockingQueue<Image> queue = new ArrayBlockingQueue<Image>(2);
+
+    private boolean running = false, cancelled = false;
+
+    public BufferedInputSource(ImageInputSource source) {
+        this.source = source;
+    }
+
+    @Override
+    public Image getNext() throws InputFailedException {
+        if (!cancelled) {
+            if (!running) {
+                running = true;
+                worker.start();
+            }
+            try {
+                return queue.take();
+            } catch (InterruptedException e) {
+                return null;
+            }
+        } else { // Cancelled
+            return null;
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        cancelled = true;
+        worker.interrupt();
+    }
+
+    private final Thread worker = new Thread() {
+        @Override
+        public void run() {
+            while (!cancelled) {
+                Image img = null;
+                try {
+                    img = source.getNext();
+                    queue.put(img);
+                } catch (InputFailedException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    // Interrupted, drop image and loop. If cancelled we will
+                    // break out.
+                    if (img != null) {
+                        img.release();
+                    }
+                }
+            }
+        }
+    };
+}
---
+++ Lib/src/test/java/uk/ac/cam/cl/wildpetscience/triton/lib/pipeline/BufferedInputSourceTest.java
@@ -0,0 +1,17 @@
+package uk.ac.cam.cl.wildpetscience.triton.lib.pipeline;
+
+import org.junit.Test;
+
+/**
+ * Tests for BufferedInputSource
+ */
+public class BufferedInputSourceTest {
+    @Test
+    public void testBufferedInputSourceCreation() {
+    }
+
+    @Test
+    public void testBufferedInputSource() {
+
+    }
+}
