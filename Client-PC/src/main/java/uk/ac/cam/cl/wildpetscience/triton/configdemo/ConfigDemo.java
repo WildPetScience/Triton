f02db73c70b663389806ae2acc38fa985c5e7497
Bruce Collie <bsc28@cam.ac.uk>
2015-01-30 16:30:26 +0000

    Start config server in demo.

--- Client-PC/src/main/java/uk/ac/cam/cl/wildpetscience/triton/configdemo/ConfigDemo.java
+++ Client-PC/src/main/java/uk/ac/cam/cl/wildpetscience/triton/configdemo/ConfigDemo.java
@@ -1,9 +1,11 @@
 package uk.ac.cam.cl.wildpetscience.triton.configdemo;
 
+import uk.ac.cam.cl.wildpetscience.triton.lib.config.ConfigServer;
+
 public class ConfigDemo {
 
     public static void main(String[] args) {
-
+        ConfigServer.start(8000);
     }
 
 }
