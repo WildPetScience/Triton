bb017953ff4cc3e888ee212199565edf21d09d04
Will Shackleton <w.shackleton@gmail.com>
2015-01-31 18:45:39 +0000

    Added documentation and checks to ImageSequenceInputSource

--- Client-PC/src/main/java/uk/ac/cam/cl/wildpetscience/triton/pipeline/ImageSequenceInputSource.java
+++ Client-PC/src/main/java/uk/ac/cam/cl/wildpetscience/triton/pipeline/ImageSequenceInputSource.java
@@ -16,25 +16,53 @@ import java.util.function.IntFunction;
 public class ImageSequenceInputSource implements ImageInputSource {
 
     private final IntFunction<File> filenameGenerator;
+    private final int delay;
 
-    public ImageSequenceInputSource(File dir, String filename_format) {
-        this(v -> new File(dir, String.format(filename_format, v)));
+    /**
+     * Creates a new ImageSequenceInputSource
+     * @param dir The directory where images are stored
+     * @param filename_format A format string to be passed to String.format with
+     *                        a single integer argument describing the
+     *                        filenames.
+     * @param delay A delay in milliseconds to add between each picture to
+     *              simulate realistic conditions.
+     */
+    public ImageSequenceInputSource(File dir, String filename_format, int delay) {
+        this(v -> new File(dir, String.format(filename_format, v)), delay);
     }
 
-    public ImageSequenceInputSource(IntFunction<File> filenameGenerator) {
+    /**
+     * Creates a new ImageSequenceInputSource
+     * @param filenameGenerator A function that generates names of files to load
+     * @param delay A delay in milliseconds to add between each picture to
+     *              simulate realistic conditions.
+     */
+    public ImageSequenceInputSource(IntFunction<File> filenameGenerator, int delay) {
         this.filenameGenerator = filenameGenerator;
+        this.delay = delay;
     }
 
     private int pos = 0;
 
+    private boolean stopped = false;
+
     @Override
     public Image getNext() throws InputFailedException {
+        if (stopped) return null;
+
         Mat mat = Highgui.imread(filenameGenerator.apply(pos++).getAbsolutePath());
+        if (mat.dataAddr() == 0) { // Decode error, according to javadoc
+            return null; // End of stream
+        }
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+        }
         return new Image(mat);
     }
 
     @Override
     public void close() throws IOException {
-
+        stopped = true;
     }
 }
