98f1a9af10eac9237ac2123a0258bffbd52fe0df
Bruce Collie <bsc28@cam.ac.uk>
2015-02-01 11:56:17 +0000

    Refactor Zone and start to build skeleton configuration manager.

--- Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/config/manage/ConfigManager.java
+++ Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/config/manage/ConfigManager.java
@@ -1,5 +1,9 @@
 package uk.ac.cam.cl.wildpetscience.triton.lib.config.manage;
 
+import uk.ac.cam.cl.wildpetscience.triton.lib.models.Zone;
+
+import java.util.List;
+
 /**
  * Static class to manage the state of a Wild Pet Science installation.
  */
@@ -10,6 +14,14 @@ public class ConfigManager {
     }
 
     public static void stopRecording() {
+
+    }
+
+    public static List<Zone> getZones() {
+        return null;
+    }
+
+    public static void setZones(List<Zone> zones) {
 
     }
 
--- Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/models/Zone.java
+++ Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/models/Zone.java
@@ -1,32 +1,13 @@
 package uk.ac.cam.cl.wildpetscience.triton.lib.models;
 
-/**
- * Represents a zone of interest within an animal's cage.
- */
 public class Zone {
 
-    public final Point origin;
-    public final Point corner;
+    public final Box area;
+    public final String id;
 
-    /**
-     * Constructs a Zone from an origin point, width and height.
-     * @param origin
-     * @param width
-     * @param height
-     */
-    public Zone(Point origin, double width, double height) {
-        this.origin = origin;
-        this.corner = new Point(origin.x + width, origin.y + height);
-    }
-
-    /**
-     * Constructs a Zone from its two opposing corners.
-     * @param origin
-     * @param corner
-     */
-    public Zone(Point origin, Point corner) {
-        this.origin = origin;
-        this.corner = corner;
+    public Zone(Box area, String id) {
+        this.area = area;
+        this.id = id;
     }
 
 }
