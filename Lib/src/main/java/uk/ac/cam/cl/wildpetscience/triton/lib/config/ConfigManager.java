5c758ed3ad1dabb82aa106d91c9e697c0716f269
Bruce Collie <bsc28@cam.ac.uk>
2015-02-01 12:01:17 +0000

    Finish skeleton manager code.

--- Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/config/ConfigManager.java
+++ Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/config/ConfigManager.java
@@ -1,5 +1,7 @@
 package uk.ac.cam.cl.wildpetscience.triton.lib.config;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import uk.ac.cam.cl.wildpetscience.triton.lib.models.LogEntry;
 import uk.ac.cam.cl.wildpetscience.triton.lib.models.Zone;
 
 import java.util.List;
@@ -10,19 +12,31 @@ import java.util.List;
 public class ConfigManager {
 
     public static void startRecording() {
-
+        throw new NotImplementedException();
     }
 
     public static void stopRecording() {
-
+        throw new NotImplementedException();
     }
 
     public static List<Zone> getZones() {
-        return null;
+        throw new NotImplementedException();
     }
 
     public static void setZones(List<Zone> zones) {
+        throw new NotImplementedException();
+    }
 
+    public static String getAccessCode() {
+        throw new NotImplementedException();
+    }
+
+    public static List<LogEntry> getSystemLogs() {
+        throw new NotImplementedException();
+    }
+
+    public static void addLogEntry(LogEntry entry) {
+        throw new NotImplementedException();
     }
 
 }
