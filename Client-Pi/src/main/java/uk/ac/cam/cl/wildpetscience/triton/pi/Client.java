cbf522abd931021feaa0eacc4b886bf9c2c797ce
Will Shackleton <w.shackleton@gmail.com>
2015-02-05 15:33:11 +0000

    Dummy commit - rerun tests

--- Client-Pi/src/main/java/uk/ac/cam/cl/wildpetscience/triton/pi/Client.java
+++ Client-Pi/src/main/java/uk/ac/cam/cl/wildpetscience/triton/pi/Client.java
@@ -3,7 +3,7 @@ package uk.ac.cam.cl.wildpetscience.triton.pi;
 import uk.ac.cam.cl.wildpetscience.triton.lib.Bootstrap;
 
 /**
- * Main entry point for Triton running on a Raspberry Pi
+ * Main entry point for Triton running on a Raspberry Pi.
  */
 public class Client {
     public static void main(String[] args) {
