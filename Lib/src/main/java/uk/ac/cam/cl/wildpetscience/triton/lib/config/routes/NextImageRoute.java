3e658392a44ce771204afee7638e20302f460914
Bruce Collie <bsc28@cam.ac.uk>
2015-02-08 18:11:35 +0000

    Add an ImageInputSource to the next image route.

--- Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/config/routes/NextImageRoute.java
+++ Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/config/routes/NextImageRoute.java
@@ -3,8 +3,11 @@ package uk.ac.cam.cl.wildpetscience.triton.lib.config.routes;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import uk.ac.cam.cl.wildpetscience.triton.lib.pipeline.ImageInputSource;
 
 public class NextImageRoute implements Route {
+
+    private ImageInputSource input;
 
     public Object handle(Request request, Response response) {
         return "";
