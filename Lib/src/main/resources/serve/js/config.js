3adf41e488427a9ce4e4d9d1dba231ef9dbb4598
Bruce Collie <bsc28@cam.ac.uk>
2015-02-08 15:51:03 +0000

    Add javascript for starting and stopping the service.

--- Lib/src/main/resources/serve/js/config.js
+++ Lib/src/main/resources/serve/js/config.js
@@ -1,3 +1,44 @@
+function createErrorMessage(message, id) {
+    return $("<p>")
+        .addClass("text-danger")
+        .attr("id", id)
+        .append("An error occurred: " + message);
+}
+
 function toggleRunning() {
-    //TODO: send start / stop request to server
+    var button = $("#toggle-button");
+    var container = $("#start-stop");
+    if(button.text() === "Start") {
+        $.ajax("/start", {
+            type: "POST",
+
+            success: function() {
+                button.text("Stop");
+                button.removeClass("btn-success");
+                button.addClass("btn-danger");
+                $("#start-error").remove();
+            },
+
+            error: function() {
+                var errorMessage = createErrorMessage("Starting the system.", "start-error");
+                container.append(errorMessage);
+            }
+        });
+    } else if(button.text() === "Stop") {
+        $.ajax("/stop", {
+            type: "POST",
+
+            success: function() {
+                button.text("Start");
+                button.removeClass("btn-danger");
+                button.addClass("btn-success");
+                $("#stop-error").remove();
+            },
+
+            error: function() {
+                var errorMessage = createErrorMessage("Stopping the system.", "stop-error");
+                container.append(errorMessage);
+            }
+        });
+    }
 }
