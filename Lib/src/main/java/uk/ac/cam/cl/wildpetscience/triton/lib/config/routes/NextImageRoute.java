ae3a0218507de4c9b82f5b0e6e05fa668646a755
Bruce Collie <bsc28@cam.ac.uk>
2015-02-08 17:46:15 +0000

    Add route to get the next image for the canvas.

---
+++ Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/config/routes/NextImageRoute.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.cl.wildpetscience.triton.lib.config.routes;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class NextImageRoute implements Route {
+
+    public Object handle(Request request, Response response) {
+        return "";
+    }
+
+}
