db0f8f58f5053e37b23e54004d7265eac8c1a363
Bruce Collie <bsc28@cam.ac.uk>
2015-02-09 09:07:09 +0000

    Add basic NextImageRoute.

--- Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/config/routes/NextImageRoute.java
+++ Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/config/routes/NextImageRoute.java
@@ -1,16 +1,25 @@
 package uk.ac.cam.cl.wildpetscience.triton.lib.config.routes;
 
+import org.opencv.core.Mat;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import uk.ac.cam.cl.wildpetscience.triton.lib.image.Image;
 import uk.ac.cam.cl.wildpetscience.triton.lib.pipeline.ImageInputSource;
+import uk.ac.cam.cl.wildpetscience.triton.lib.pipeline.InputFailedException;
 
 public class NextImageRoute implements Route {
 
     private ImageInputSource input;
 
     public Object handle(Request request, Response response) {
-        return "";
+        try {
+            Image image = input.getNext();
+            return "";
+        } catch(InputFailedException e) {
+            response.status(500);
+            return "";
+        }
     }
 
 }
