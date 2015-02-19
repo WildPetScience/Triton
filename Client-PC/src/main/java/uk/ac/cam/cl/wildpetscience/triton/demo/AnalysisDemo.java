3101b92b05cb247fb5ebf8deb6a733e7cfd7b78f
Will Shackleton <w.shackleton@gmail.com>
2015-02-19 14:02:56 +0000

    Added AnalysisDemo to demonstrate output of analysis stage combined with other stages

---
+++ Client-PC/src/main/java/uk/ac/cam/cl/wildpetscience/triton/demo/AnalysisDemo.java
@@ -0,0 +1,59 @@
+package uk.ac.cam.cl.wildpetscience.triton.demo;
+
+import uk.ac.cam.cl.wildpetscience.triton.lib.image.Image;
+import uk.ac.cam.cl.wildpetscience.triton.lib.models.AnimalPosition;
+import uk.ac.cam.cl.wildpetscience.triton.lib.models.ConfigData;
+import uk.ac.cam.cl.wildpetscience.triton.lib.pipeline.Driver;
+import uk.ac.cam.cl.wildpetscience.triton.lib.pipeline.ImageInputSource;
+import uk.ac.cam.cl.wildpetscience.triton.lib.pipeline.OutputSink;
+import uk.ac.cam.cl.wildpetscience.triton.lib.pipeline.PassthroughFilter;
+import uk.ac.cam.cl.wildpetscience.triton.lib.pipeline.analysis.Analysis;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.function.Function;
+
+public class AnalysisDemo {
+    private final JFrame frame;
+    private final Driver driver;
+
+    public AnalysisDemo(Function<OutputSink<PassthroughFilter.Passthrough<Image, AnimalPosition>>,
+            Driver<Image>> creator,
+                        ConfigData configData,
+                        Analysis analysis,
+                        String title) {
+        frame = new JFrame(title);
+        frame.setSize(640, 480);
+
+        frame.setResizable(true);
+        frame.setLocationRelativeTo(null);
+
+        AnalysisTestPanel panel = new AnalysisTestPanel(configData, analysis);
+        frame.getContentPane().add(panel);
+
+        driver = creator.apply(panel);
+
+        frame.addWindowListener(new WindowListener() {
+            @Override public void windowOpened(WindowEvent windowEvent) { }
+
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                driver.cancel();
+            }
+
+            @Override public void windowClosed(WindowEvent windowEvent) {  }
+            @Override public void windowIconified(WindowEvent windowEvent) {  }
+            @Override public void windowDeiconified(WindowEvent windowEvent) {  }
+            @Override public void windowActivated(WindowEvent windowEvent) {  }
+            @Override public void windowDeactivated(WindowEvent windowEvent) {  }
+        });
+    }
+
+    public void start() {
+        EventQueue.invokeLater(() -> frame.setVisible(true));
+
+        driver.start();
+    }
+}
