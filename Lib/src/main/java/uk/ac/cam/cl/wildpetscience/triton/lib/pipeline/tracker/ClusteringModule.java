3b5ca9ffab3e5c846d76187c286d7b7ac9c43a38
gavrilavlad <vlad.gavrila.18@gmail.com>
2015-02-19 12:29:39 +0000

    Clustering Algorithm
    - Processing done in grayscale for performance
    - Fixed allocation issue

--- Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/pipeline/tracker/ClusteringModule.java
+++ Lib/src/main/java/uk/ac/cam/cl/wildpetscience/triton/lib/pipeline/tracker/ClusteringModule.java
@@ -17,35 +17,42 @@ import static org.opencv.imgproc.Imgproc.*;
 
 //
 public class ClusteringModule {
-    private double prob;
-    private boolean[][] used, movement;
+    private static double prob;
+    private static boolean[][] used, movement;
 
-    private int area, maxArea, totalArea;
-    private int rows, cols;
-    private final int vicinity = 1;
+    private static int area, maxArea, totalArea;
+    private static int rows, cols;
+    private static final int vicinity = 1;
 
-    private int xCurr, yCurr, xAns, yAns;
+    private static int xCurr, yCurr, xAns, yAns;
 
-    private Mat image;
+    private static Mat image;
 
-    public ClusteringModule(Mat img) {
+    private ClusteringModule(Mat img) {
+    }
+
+    public static void Process(Mat img) {
         totalArea = 0;
         prob = 0.0;
-        rows = img.rows();
-        cols = img.cols();
+
+        if (rows != img.rows() || cols != img.cols()) {
+            rows = img.rows();
+            cols = img.cols();
+
+            used = new boolean[rows][cols];
+            movement = new boolean[rows][cols];
+        }
 
         xAns = 0;
         yAns = 0;
+        maxArea = 0;
+        prob = 0;
         image = img;
 
-        if(rows == 0 || cols == 0)
+        if (rows == 0 || cols == 0)
             return;
 
-        used = new boolean[rows][cols];
-        movement = new boolean[rows][cols];
-
-        dilate(img, img, getStructuringElement(MORPH_RECT, new Size(50, 50)));
-        erode(img, img, getStructuringElement(MORPH_RECT, new Size(20, 20)));
+        dilate(img, img, getStructuringElement(MORPH_RECT, new Size(20, 20)));
 
         for(int i = 0; i < rows; ++i)
             for(int j = 0; j < cols; ++j) {
@@ -76,7 +83,7 @@ public class ClusteringModule {
 
     }
 
-    private class IntPoint {
+    private static class IntPoint {
         public int x, y;
         public IntPoint(int x, int y) {
             this.x = x;
@@ -84,7 +91,7 @@ public class ClusteringModule {
         }
     }
 
-    private void bfs(int y, int x) {
+    private static void bfs(int y, int x) {
         Queue<IntPoint> elements = new LinkedList<IntPoint>();
 
         elements.add(new IntPoint(x, y));
@@ -135,24 +142,23 @@ public class ClusteringModule {
         yCurr = (2*yCurr + yMin + yMax) / 4;
     }
 
-    double getX() {
+    public static double getX() {
         if(maxArea == 0 || cols == 0)
             return 0.0;
         return 1.0 * xAns / cols;
     }
 
-    double getY() {
+    public static double getY() {
         if(maxArea == 0 || cols == 0)
             return 0.0;
         return 1.0 * yAns / rows;
     }
 
-    double getConfidence() {
+    public static double getConfidence() {
         if(maxArea == 0 || cols == 0)
             return 0.0;
         double prob = 1.0 * maxArea / totalArea;
 
-        System.out.println(maxArea + " " + totalArea);
         if(maxArea < 1500)
             prob = prob / 4;
         if(maxArea < 2000)
